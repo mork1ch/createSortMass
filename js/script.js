a2e1a07b0bb2ba239822b6cfef5910619b72c2e5
Mork - Юрий <mork.florik@gmail.com>
2020-02-25 14:27:18 +0300

    полностью выполненное задание

--- js/script.js
+++ js/script.js
@@ -3,21 +3,34 @@
 function sortirovka() {
     var razmer = +prompt("Дайте размер массива")
     var masiv = []
-    
+
     var max = document.getElementById('max')
     var min = document.getElementById('min')
 
-    console.log(max.value , min.value , razmer);
-    console.log(typeof(razmer))
+    console.log(max.value, min.value, razmer);
+    console.log(typeof (razmer))
+
+    let tablece = "<table align='center' border='1' cellspacing='0'>";
+
     for (var i = 0; i < razmer; i++) {
         masiv.push(parseInt(Math.random() * (parseInt(max.value) - parseInt(min.value)) + parseInt(min.value)));
+
+        tablece = tablece + "<td style='height:15px; width:15px; text-align:center'>" + masiv[i] + "</td>";
     }
-    document.getElementById("masiv").innerHTML = masiv;
-    
-    masiv.sort(function(a,b){
-        return a - b ;
+
+    //document.write(tablece);
+
+    document.getElementById("masiv").innerHTML = tablece
+
+    masiv.sort(function (a, b) {
+        return a - b;
     });
 
-    document.getElementById("otsort").innerHTML = masiv;
+    let ots = "<table align='center' border='1' cellspacing='0'>";
 
+    for (i = 0; i < razmer; i ++){
+        ots = ots + "<td style='height:15px; width:15px; text-align:center'>" + masiv[i] + "</td>";
+    }
+
+    document.getElementById("otsort").innerHTML = ots
 }
